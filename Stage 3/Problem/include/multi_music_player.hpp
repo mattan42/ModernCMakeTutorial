347c53fa1911916a24da65eb59aa45b4878812f8
Mattan <mattanharnoy@gmail.com>
2025-06-19 14:58:22 +0300

    Added a multi music player header and soruce

---
+++ Stage 3/Problem/include/multi_music_player.hpp
@@ -0,0 +1,49 @@
+#ifndef MULTI_MUSIC_PLAYER
+#define MULTI_MUSIC_PLAYER
+
+#include <string>
+#include "spotify_player/spotify_player.hpp"
+#include "music_player/music_player.hpp"
+
+/**
+ * @brief This class can play music from several different music sources.
+ * 
+ * @note Currently only supporting spotify.
+ */
+class MultiMusicPlayer : music_player::MusicPlayer
+{
+public:
+    enum class Source
+    {
+        spotify,
+    };
+
+    MultiMusicPlayer(Source source);
+
+    /**
+     * @brief Tries searching for track of name.
+     * 
+     * @return true on success, false otherwise.
+     */
+    bool search_track(std::string track_name);
+
+    /**
+     * @brief Tries enabling bluetooth for the current source.
+     * 
+     * @return true on sucess, false if the current source doesn't support bluetooth.
+     */
+    bool enable_bluetooth();
+
+    /**
+     * @return True if the current source supports bluetooth.
+     */
+    bool source_supports_bluetooth();
+        
+    virtual bool play_music() override;
+
+private:
+    spotify_player::SpotifyPlayer spotify_player;
+    Source current_source;
+};
+
+#endif
---
+++ Stage 3/Problem/src/multi_music_player.cpp
@@ -0,0 +1,41 @@
+#include "multi_music_player.hpp"
+
+MultiMusicPlayer::MultiMusicPlayer(Source source) :
+    spotify_player(),
+    current_source(source)
+{
+    if (source != Source::spotify)
+    {
+        throw "Currently not supporting sources other than spotify";
+    }
+}
+
+bool MultiMusicPlayer::search_track(std::string track_name)
+{
+    // Dummy implementaiton
+    this->spotify_player.set_track_id(8);
+}
+
+bool MultiMusicPlayer::enable_bluetooth()
+{
+#ifdef SPOTIFY_ENABLE_BLUETOOTH
+    this->spotify_player.enable_bluetooth();
+    return true;
+#endif
+
+    return false;
+}
+
+bool MultiMusicPlayer::source_supports_bluetooth()
+{
+#ifdef SPOTIFY_ENABLE_BLUETOOTH
+    return true;
+#endif
+
+    return false;
+}
+    
+bool MultiMusicPlayer::play_music()
+{
+    return this->spotify_player.play_music();
+}
