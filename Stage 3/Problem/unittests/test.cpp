1e5df93dcea1e55acb460a1d7dbac1c8f2f4d318
Mattan <mattanharnoy@gmail.com>
2025-06-19 15:07:45 +0300

    Written unittests for stage 3 executable

--- Stage 3/Problem/unittests/test.cpp
+++ Stage 3/Problem/unittests/test.cpp
@@ -1,5 +1,31 @@
 #include <gtest/gtest.h>
+#include "multi_music_player.hpp"
 
-TEST(TestSuiteName, TestName) {
-    // EXPECT_TRUE()
+TEST(MultiMusicPlayerSpotifyTests, Sanity)
+{
+    MultiMusicPlayer multi_music_player(MultiMusicPlayer::Source::spotify);
 }
+
+TEST(MultiMusicPlayerSpotifyTests, NormalUsage)
+{
+    MultiMusicPlayer multi_music_player(MultiMusicPlayer::Source::spotify);
+
+    EXPECT_TRUE(multi_music_player.search_track("I bet that you look good on the dancefloor - arctic monkeys"));
+    EXPECT_TRUE(multi_music_player.play_music());
+}
+
+#ifdef SPOTIFY_ENABLE_BLUETOOTH
+TEST(MultiMusicPlayerSpotifyTests, Bluetooth)
+{
+    MultiMusicPlayer multi_music_player(MultiMusicPlayer::Source::spotify);
+
+    EXPECT_TRUE(multi_music_player.enable_bluetooth());
+}
+#else
+TEST(MultiMusicPlayerSpotifyTests, NoBluetooth)
+{
+    MultiMusicPlayer multi_music_player(MultiMusicPlayer::Source::spotify);
+
+    EXPECT_FALSE(multi_music_player.enable_bluetooth());
+}
+#endif
